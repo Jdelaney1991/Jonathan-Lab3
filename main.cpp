4ee48605d9be97e525f3f8b0d87527c2d0a60671
Jdelaney1991 <delaneyep91@gmail.com>
2023-07-21 02:46:04 +0000

    CA1-Question 5
    Signed-off-by: Jdelaney1991 <delaneyep91@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
-
 #include "mbed.h"
 #include "LSM6DSLSensor.h"
 
 #define PI 3.141592654
 
-static DevI2C devI2c(PB_11,PB_10);
-static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5); // high address
+static DevI2C devI2c(PB_11, PB_10);
+static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5); // high address
 
+DigitalOut led1(LED1);
 
-float computeAngle(int x, int y, int z){
+float computeAngle(int x, int y, int z) {
     float res = 0;
+
+    // Calculate the angle using accelerometer data
+    // You can write your angle calculation logic here based on x, y, and z values
 
     return res;
 }
@@ -18,7 +21,7 @@ float computeAngle(int x, int y, int z){
 int main() {
     uint8_t id;
     int32_t axes[3];
-    float res=0;
+    float res = 0;
     acc_gyro.init(NULL);
 
     acc_gyro.enable_x();
@@ -28,14 +31,19 @@ int main() {
     acc_gyro.read_id(&id);
     printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
-    while(1) {
+    while (1) {
 
         acc_gyro.get_x_axes(axes);
         res = computeAngle(axes[0], axes[1], axes[2]);
         printf("LSM6DSL: %6d, %6d, %6d, %3.2f\r\n", axes[0], axes[1], axes[2], res);
 
+        // Check if the board is lying flat (you may need to adjust the threshold values based on your observations)
+        if (axes[0] > 900 && axes[0] < 1100 && axes[1] > 900 && axes[1] < 1100 && axes[2] > 19000 && axes[2] < 21000) {
+            led1 = 1; // Turn ON LED1 (green LED) when the board is lying flat
+        } else {
+            led1 = 0; // Turn OFF LED1 otherwise
+        }
 
         thread_sleep_for(2000);
-
     }
 }
